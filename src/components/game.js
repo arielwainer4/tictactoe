f25d84b3e1ec10d4023ffd982185b0cb3dc86e5f
Ariel Wainer <arielwainer4@gmail.com>
2020-10-22 16:29:55 -0400

    Feat: file setup

---
+++ src/components/game.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Game = () => {
+  return (
+    <div>
+      Game Here
+    </div>
+  )
+}
+
+export default Game
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Board} from './board'
+export {default as Square} from './square'
+export {default as Game} from './game'
+
+
